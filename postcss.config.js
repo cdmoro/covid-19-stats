f93b681a9a246fa8913e416338bb45d8bd9bc010
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-03-18 22:26:15 -0300

    PurgeCSS :tada:

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
-const tailwindcss = require('tailwindcss')
-
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('autoprefixer')
+        require('tailwindcss'),
+        require('autoprefixer'),
+        process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+            content: [
+                './src/**/*.tsx',
+                './public/index.html'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
