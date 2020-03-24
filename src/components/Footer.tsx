f0fefc0334b2f659369ae0ed424f4e73e5ef3b67
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-03-24 18:23:00 -0300

    Fix underline links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer: FC = () => {
     return (
         <div className="Footer mt-2 text-xs text-muted text-center">
             <div className="flex flex-col md:flex-row justify-center items-center mt-2">
-                <div>Powered by <a className="hover:underline" href="https://github.com/mathdroid/covid-19-api">COVID-19 API</a></div>
+                <div>Powered by <a className="underline hover:no-underline" href="https://github.com/mathdroid/covid-19-api">COVID-19 API</a></div>
                 <div className="hidden md:inline-block px-1">|</div>
                 <div>
-                    Made by <a className="hover:underline" href="https://www.linkedin.com/in/cdbonadeo/" target="_blank" rel="noopener noreferrer">Carlos Bonadeo</a>
+                    Made by <a className="underline hover:no-underline" href="https://www.linkedin.com/in/cdbonadeo/" target="_blank" rel="noopener noreferrer">Carlos Bonadeo</a>
                     <div className="md:hidden inline-block px-1">|</div>
-                    <a className="md:hidden inline-block hover:underline" href="https://github.com/cdmoro/covid-19-stats">GitHub</a>
+                    <a className="md:hidden inline-block underline hover:no-underline" href="https://github.com/cdmoro/covid-19-stats">GitHub</a>
                 </div>
             </div>
         </div>
