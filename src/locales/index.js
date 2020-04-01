0d498ba1a07184a213ce323e7b36a5fe28d4e22b
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-04-01 11:26:27 -0300

    Clean up translations

--- src/locales/index.js
+++ src/locales/index.js
@@ -7,7 +7,7 @@ export default {
       recovered: 'Recovered',
       deaths: 'Deaths',
       'last-updated': 'Last updated',
-      'powered-by': 'Thanks to',
+      'powered-by': 'Provided by',
       'made-by': 'Made by',
       'change-theme': 'Change theme',
       'reset-position': 'Reset position',
@@ -25,14 +25,14 @@ export default {
       recovered: 'Recuperados',
       deaths: 'Muertes',
       'last-updated': 'Última actualización',
-      'powered-by': 'Gracias a',
+      'powered-by': 'Proporcionado por',
       'made-by': 'Hecho por',
       'change-theme': 'Cambiar tema',
       'reset-position': 'Posición inicial',
       'automatic-manual-zoom': 'Zoom automático/manual',
       'zoom-in': 'Acercar',
       'zoom-out': 'Alejar',
-      'world-data': 'World data'
+      'world-data': 'Datos mundiales'
     }
   },
   pt: {
@@ -43,14 +43,14 @@ export default {
       recovered: 'Recuperado',
       deaths: 'Mortes',
       'last-updated': 'Ultima atualização',
-      'powered-by': 'Graças a',
+      'powered-by': 'Fornecido por',
       'made-by': 'Feito por',
       'change-theme': 'Mudar tema',
       'reset-position': 'Repor posição',
       'automatic-manual-zoom': 'Zoom automático/manual',
       'zoom-in': 'Mais zoom',
       'zoom-out': 'Reduzir o zoom',
-      'world-data': 'World data'
+      'world-data': 'Dados mundiais'
     }
   },
   it: {
@@ -61,14 +61,14 @@ export default {
       recovered: 'Recuperato',
       deaths: 'Morti',
       'last-updated': 'Ultimo aggiornamiento',
-      'powered-by': 'Grazie a',
+      'powered-by': 'Fornito da',
       'made-by': 'Fatto da',
       'change-theme': 'Cambia tema',
       'reset-position': 'Ripristina posizione',
       'automatic-manual-zoom': 'Zoom automatico/manuale',
       'zoom-in': 'Ingrandire',
       'zoom-out': 'Rimpicciolisci',
-      'world-data': 'World data'
+      'world-data': 'Dati mondiali'
     }
   },
   fr: {
@@ -79,14 +79,14 @@ export default {
       recovered: 'Rétabli',
       deaths: 'Morts',
       'last-updated': 'Dernière mise à jour',
-      'powered-by': 'Grâce à',
+      'powered-by': 'Fourni par',
       'made-by': 'Faite par',
       'change-theme': 'Change le thème',
       'reset-position': 'Réinitialiser la position',
       'automatic-manual-zoom': 'Zoom automatique/manuel',
       'zoom-in': 'Agrandir',
       'zoom-out': 'Dézoomer',
-      'world-data': 'World data'
+      'world-data': 'Données mondiales'
     }
   },
   ch: {
@@ -97,14 +97,14 @@ export default {
       recovered: '已恢复',
       deaths: '死亡人数',
       'last-updated': '最近更新时间',
-      'powered-by': '谢谢',
+      'powered-by': '由...提供',
       'made-by': '由制成',
       'change-theme': '改变主题',
       'reset-position': '重置位置',
       'automatic-manual-zoom': '自动/手动变焦',
       'zoom-in': '放大',
       'zoom-out': '缩小',
-      'world-data': '全球'
+      'world-data': '世界数据'
     }
   },
   gr: {
@@ -115,14 +115,14 @@ export default {
       recovered: 'Ανακτηθεί',
       deaths: 'Θάνατοι',
       'last-updated': 'Τελευταία ενημέρωση',
-      'powered-by': 'Χάρη σε',
+      'powered-by': 'Παρέχεται από',
       'made-by': 'Φτιαγμένο από',
       'change-theme': 'Αλλαξε θέμα',
       'reset-position': 'Επαναφορά θέσης',
       'automatic-manual-zoom': 'Αυτόματο/χειροκίνητο ζουμ',
       'zoom-in': 'Μεγέθυνση',
       'zoom-out': 'Σμίκρυνση',
-      'world-data': 'World data'
+      'world-data': 'Παγκόσμια δεδομένα'
     }
   },
   ru: {
@@ -133,7 +133,7 @@ export default {
       recovered: 'Выздоровевшие',
       deaths: 'Смертность',
       'last-updated': 'Последнее обновление',
-      'powered-by': 'Благодаря',
+      'powered-by': 'Предоставлено',
       'made-by': 'Сделанно в',
       'change-theme': 'Поменять тематику',
       'reset-position': 'Сбросить позицию',
