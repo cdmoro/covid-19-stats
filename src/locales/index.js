c4aa6c4fe1996388215f2d49c33b6b74b5199fc7
Anya <anya.p.nguyen@gmail.com>
2020-03-31 23:53:22 -0700

    added russian and vietnamese local, changed world data header to local language

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,121 +1,164 @@
 export default {
-    en: {
-        name: 'English',
-        translation: {
-            "title": "COVID-19 stats",
-            "confirmed": "Confirmed",
-            "recovered": "Recovered",
-            "deaths": "Deaths",
-            "last-updated": "Last updated",
-            "powered-by": "Thanks to",
-            "made-by": "Made by",
-            "change-theme": "Change theme",
-            "reset-position": "Reset position",
-            "automatic-manual-zoom": "Automatic/manual zoom",
-            "zoom-in": "Zoom in",
-            "zoom-out": "Zoom out"
-        }
-    },
-    es: {
-        name: 'Spanish',
-        translation: {
-            "title": "Estadísticas del COVID-19",
-            "confirmed": "Confirmados",
-            "recovered": "Recuperados",
-            "deaths": "Muertes",
-            "last-updated": "Última actualización",
-            "powered-by": "Gracias a",
-            "made-by": "Hecho por",
-            "change-theme": "Cambiar tema",
-            "reset-position": "Posición inicial",
-            "automatic-manual-zoom": "Zoom automático/manual",
-            "zoom-in": "Acercar",
-            "zoom-out": "Alejar"
-        }
-    },
-    pt: {
-        name: 'Portuguese',
-        translation: {
-            "title": "Estatísticas do COVID-19",
-            "confirmed": "Confirmado",
-            "recovered": "Recuperado",
-            "deaths": "Mortes",
-            "last-updated": "Ultima atualização",
-            "powered-by": "Graças a",
-            "made-by": "Feito por",
-            "change-theme": "Mudar tema",
-            "reset-position": "Repor posição",
-            "automatic-manual-zoom": "Zoom automático/manual",
-            "zoom-in": "Mais zoom",
-            "zoom-out": "Reduzir o zoom"
-        }
-    },
-    it: {
-        name: 'Italian',
-        translation: {
-            "title": "Statistiche COVID-19",
-            "confirmed": "Confermato",
-            "recovered": "Recuperato",
-            "deaths": "Morti",
-            "last-updated": "Ultimo aggiornamiento",
-            "powered-by": "Grazie a",
-            "made-by": "Fatto da",
-            "change-theme": "Cambia tema",
-            "reset-position": "Ripristina posizione",
-            "automatic-manual-zoom": "Zoom automatico/manuale",
-            "zoom-in": "Ingrandire",
-            "zoom-out": "Rimpicciolisci"
-        }
-    },
-    fr: {
-        name: 'French',
-        translation: {
-            "title": "Statistiques COVID-19",
-            "confirmed": "Confirmé",
-            "recovered": "Rétabli",
-            "deaths": "Morts",
-            "last-updated": "Dernière mise à jour",
-            "powered-by": "Grâce à",
-            "made-by": "Faite par",
-            "change-theme": "Change le thème",
-            "reset-position": "Réinitialiser la position",
-            "automatic-manual-zoom": "Zoom automatique/manuel",
-            "zoom-in": "Agrandir",
-            "zoom-out": "Dézoomer"
-        }
-    },
-    ch: {
-        name: 'Chinese',
-        translation: {
-            "title": "COVID-19统计",
-            "confirmed": "已确认",
-            "recovered": "已恢复",
-            "deaths": "死亡人数",
-            "last-updated": "最近更新时间",
-            "powered-by": "谢谢",
-            "made-by": "由制成",
-            "change-theme": "改变主题",
-            "reset-position": "重置位置",
-            "automatic-manual-zoom": "自动/手动变焦",
-            "zoom-in": "放大",
-            "zoom-out": "缩小"
-        }
-    },
-    gr: {
-        name: 'Greek',
-        translation: {
-            "title": "COVID-19 στατιστικά στοιχεία",
-            "confirmed": "Επιβεβαιωμένος",
-            "recovered": "Ανακτηθεί",
-            "deaths": "Θάνατοι",
-            "last-updated": "Τελευταία ενημέρωση",
-            "powered-by": "Χάρη σε",
-            "made-by": "Φτιαγμένο από",
-            "change-theme": "Αλλαξε θέμα",
-            "reset-position": "Επαναφορά θέσης",
-            "automatic-manual-zoom": "Αυτόματο/χειροκίνητο ζουμ",
-            "zoom-in": "Μεγέθυνση",
-            "zoom-out": "Σμίκρυνση"
-        }
+  en: {
+    name: 'English',
+    translation: {
+      title: 'COVID-19 stats',
+      confirmed: 'Confirmed',
+      recovered: 'Recovered',
+      deaths: 'Deaths',
+      'last-updated': 'Last updated',
+      'powered-by': 'Thanks to',
+      'made-by': 'Made by',
+      'change-theme': 'Change theme',
+      'reset-position': 'Reset position',
+      'automatic-manual-zoom': 'Automatic/manual zoom',
+      'zoom-in': 'Zoom in',
+      'zoom-out': 'Zoom out',
+      'world-data': 'World data'
     }
+  },
+  es: {
+    name: 'Spanish',
+    translation: {
+      title: 'Estadísticas del COVID-19',
+      confirmed: 'Confirmados',
+      recovered: 'Recuperados',
+      deaths: 'Muertes',
+      'last-updated': 'Última actualización',
+      'powered-by': 'Gracias a',
+      'made-by': 'Hecho por',
+      'change-theme': 'Cambiar tema',
+      'reset-position': 'Posición inicial',
+      'automatic-manual-zoom': 'Zoom automático/manual',
+      'zoom-in': 'Acercar',
+      'zoom-out': 'Alejar',
+      'world-data': 'World data'
+    }
+  },
+  pt: {
+    name: 'Portuguese',
+    translation: {
+      title: 'Estatísticas do COVID-19',
+      confirmed: 'Confirmado',
+      recovered: 'Recuperado',
+      deaths: 'Mortes',
+      'last-updated': 'Ultima atualização',
+      'powered-by': 'Graças a',
+      'made-by': 'Feito por',
+      'change-theme': 'Mudar tema',
+      'reset-position': 'Repor posição',
+      'automatic-manual-zoom': 'Zoom automático/manual',
+      'zoom-in': 'Mais zoom',
+      'zoom-out': 'Reduzir o zoom',
+      'world-data': 'World data'
+    }
+  },
+  it: {
+    name: 'Italian',
+    translation: {
+      title: 'Statistiche COVID-19',
+      confirmed: 'Confermato',
+      recovered: 'Recuperato',
+      deaths: 'Morti',
+      'last-updated': 'Ultimo aggiornamiento',
+      'powered-by': 'Grazie a',
+      'made-by': 'Fatto da',
+      'change-theme': 'Cambia tema',
+      'reset-position': 'Ripristina posizione',
+      'automatic-manual-zoom': 'Zoom automatico/manuale',
+      'zoom-in': 'Ingrandire',
+      'zoom-out': 'Rimpicciolisci',
+      'world-data': 'World data'
+    }
+  },
+  fr: {
+    name: 'French',
+    translation: {
+      title: 'Statistiques COVID-19',
+      confirmed: 'Confirmé',
+      recovered: 'Rétabli',
+      deaths: 'Morts',
+      'last-updated': 'Dernière mise à jour',
+      'powered-by': 'Grâce à',
+      'made-by': 'Faite par',
+      'change-theme': 'Change le thème',
+      'reset-position': 'Réinitialiser la position',
+      'automatic-manual-zoom': 'Zoom automatique/manuel',
+      'zoom-in': 'Agrandir',
+      'zoom-out': 'Dézoomer',
+      'world-data': 'World data'
+    }
+  },
+  ch: {
+    name: 'Chinese',
+    translation: {
+      title: 'COVID-19统计',
+      confirmed: '已确认',
+      recovered: '已恢复',
+      deaths: '死亡人数',
+      'last-updated': '最近更新时间',
+      'powered-by': '谢谢',
+      'made-by': '由制成',
+      'change-theme': '改变主题',
+      'reset-position': '重置位置',
+      'automatic-manual-zoom': '自动/手动变焦',
+      'zoom-in': '放大',
+      'zoom-out': '缩小',
+      'world-data': '全球'
+    }
+  },
+  gr: {
+    name: 'Greek',
+    translation: {
+      title: 'COVID-19 στατιστικά στοιχεία',
+      confirmed: 'Επιβεβαιωμένος',
+      recovered: 'Ανακτηθεί',
+      deaths: 'Θάνατοι',
+      'last-updated': 'Τελευταία ενημέρωση',
+      'powered-by': 'Χάρη σε',
+      'made-by': 'Φτιαγμένο από',
+      'change-theme': 'Αλλαξε θέμα',
+      'reset-position': 'Επαναφορά θέσης',
+      'automatic-manual-zoom': 'Αυτόματο/χειροκίνητο ζουμ',
+      'zoom-in': 'Μεγέθυνση',
+      'zoom-out': 'Σμίκρυνση',
+      'world-data': 'World data'
+    }
+  },
+  ru: {
+    name: 'Russian',
+    translation: {
+      title: 'COVID-19 статистика',
+      confirmed: 'Подтвержденные случаи',
+      recovered: 'Выздоровевшие',
+      deaths: 'Смертность',
+      'last-updated': 'Последнее обновление',
+      'powered-by': 'Благодаря',
+      'made-by': 'Сделанно в',
+      'change-theme': 'Поменять тематику',
+      'reset-position': 'Сбросить позицию',
+      'automatic-manual-zoom': 'Автоматическое/ручное увеличение',
+      'zoom-in': 'Приблизить',
+      'zoom-out': 'Уменьшить',
+      'world-data': 'Глобально'
+    }
+  },
+  vn: {
+    name: 'Vietnamese',
+    translation: {
+      title: 'COVID-19 số liệu thống kê',
+      confirmed: 'Xác nhận',
+      recovered: 'Phục hồi',
+      deaths: 'Tử vong',
+      'last-updated': 'Cập nhật mới nhất',
+      'powered-by': 'Cung cấp bởi',
+      'made-by': 'Được làm bởi',
+      'change-theme': 'Thay đổi nền',
+      'reset-position': 'Về vị trí ban đầu',
+      'automatic-manual-zoom': 'Phóng to/thu nhỏ tự động/bằng tay',
+      'zoom-in': 'Phóng to',
+      'zoom-out': 'Thu nhỏ',
+      'world-data': 'Toàn cầu'
+    }
+  }
 }
