646c726419661bf20fba39f92fb25ec0c4e46654
Carlos <carlosbonadeo@gmail.com>
2022-02-13 11:07:20 -0300

    deploy

---
+++ precache-manifest.77734993d4ee66ec8de0b55a316a73ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9407af226a2f92e11fad4a1f870184c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08b77384efe6ee39ad9a",
+    "url": "./static/css/2.4e815b96.chunk.css"
+  },
+  {
+    "revision": "4ac323a38aa22d8f64c2",
+    "url": "./static/css/main.2d4e39db.chunk.css"
+  },
+  {
+    "revision": "08b77384efe6ee39ad9a",
+    "url": "./static/js/2.eb5e726d.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "./static/js/2.eb5e726d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac323a38aa22d8f64c2",
+    "url": "./static/js/main.61eda91e.chunk.js"
+  },
+  {
+    "revision": "d29a336a6ce5e7c16f8d",
+    "url": "./static/js/runtime-main.dd8e06fa.js"
+  },
+  {
+    "revision": "7215a9bdfac69f0d0a71cdb53d315bf0",
+    "url": "./static/media/github.7215a9bd.svg"
+  }
+]);
